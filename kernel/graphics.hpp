80f9427f9fda06ab94a2449d5d1d36fb4a0da03b
Saza-ku <the.latticeheart@gmail.com>
2021-04-28 09:21:10 +0000

    5.5まで

---
+++ kernel/graphics.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "frame_buffer_config.hpp"
+
+struct PixelColor {
+  uint8_t r, g, b;
+};
+
+class PixelWriter {
+  public:
+    // コンストラクタ: 初期化子リストで config_ を初期化
+    PixelWriter(const FrameBufferConfig& config) : config_{config} {
+    }
+    // デストラクタ: 明示的なデフォルト定義
+    virtual ~PixelWriter() = default;
+    // 純粋仮想関数
+    virtual void Write(int x, int y, const PixelColor& c) = 0;
+
+  // protected はサブクラスからの参照をできるようにする
+  protected:
+    uint8_t* PixelAt(int x, int y) {
+      return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+    }
+
+  private:
+    const FrameBufferConfig& config_;
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    // 継承コンストラクタ: 親クラスのコンストラクタを使えるようにする
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override;
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    // 親クラスのコンストラクタを使えるようにする
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override;
+};
---
+++ kernel/newlib_support.c
@@ -0,0 +1,5 @@
+#include <sys/types.h>
+
+caddr_t sbrk(int iincr) {
+    return NULL;
+}
