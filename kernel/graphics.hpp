8f957abe29b18145454d5393322abe768b3784b9
Saza-ku <the.latticeheart@gmail.com>
2022-02-17 13:37:46 +0000

    9章までできた

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include "frame_buffer_config.hpp"
 
 struct PixelColor {
@@ -34,12 +35,12 @@ auto operator +(const Vector2D<T>& lhs, const Vector2D<U>& rhs)
 
 template <typename T>
 Vector2D<T> ElementMax(const Vector2D<T>& lhs, const Vector2D<T>& rhs) {
-  return {std::max(lhs.x, rhs.x), std::max(lhs.y, rhs.t)};
+  return {std::max(lhs.x, rhs.x), std::max(lhs.y, rhs.y)};
 }
 
 template <typename T>
 Vector2D<T> ElementMin(const Vector2D<T>& lhs, const Vector2D<T>& rhs) {
-  return {std::min(lhs.x, rhs.x), std::min(lhs.y, rhs.t)};
+  return {std::min(lhs.x, rhs.x), std::min(lhs.y, rhs.y)};
 }
 
 template <typename T>
